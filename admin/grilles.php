db24b4bfe25277eec0281df7851e13789228feeb
atm-greg <gregory.blemand@atm-consulting.fr>
2018-07-11 08:32:11 +0200

    oups !

--- admin/grilles.php
+++ admin/grilles.php
@@ -227,4 +227,97 @@ if(count($TTransport))
             }
         }
         
+//         $sql = "SELECT * FROM ".MAIN_DB_PREFIX."c_grilles_transporteurs WHERE fk_trans = ".$tid;
+//         $res = $db->query($sql);
+//         if($res)
+//         {
+//             if($db->num_rows($res)) { 
+                
+//                 print_titre($label);
+//                 $TData = array();
+                
+// //                 while ($obj = $db->fetch_object($res))
+// //                 {
+// //                     $TData[$obj->fk_pays][$obj->departement][$obj->zipcode][$obj->poids] = $obj->tarif;
+// //                 }
+                
+                 
+//                 	$pays = 0;
+//                 	while ($obj = $db->fetch_object($res))
+//                 	{
+//                 	    $pays = $obj->fk_pays;
+//                 	    $dpt = $obj->departement;
+//                 	    $zip = $obj->zipcode;
+//                 	    $TData[$obj->fk_pays][$obj->departement][$obj->zipcode][$obj->poids] = $obj->tarif;
+//                 	}
+//                 	$entete = false;
+//                 	foreach ($TData as $pays => $depts)
+//                 	{ 
+//                 	    foreach ($depts as $dpt => $zip)
+//                     	{ 
+//                     	   foreach ($zip as $code => $prices)
+//                     	   {
+//                     	       if(!$entete)
+//                     	       {
+//                     	           ?>
+<!--                 <table class="noborder" width="100%"> -->
+<!--                 	<tr class="liste_titre"> -->
+<!--                 		<td align="left">Pays</td> -->
+<!--                 		<td align="center">Département</td> -->
+<!--                 		<td align="center">Code postal</td> -->
+<!--                 		<td align="center">Timbre</td> -->
+                		<?php 
+//                 		$oldpds = 0;
+//                     	foreach ($prices as $pds => $prix){
+//                     	    ?>
+                    	    <!-- <td align="center"><?php //echo 'de '. $oldpds . ' à '; ?><input type="text" name="newPalier[[view.contrat]]" value="<?php //echo $pds ?>" size="5" />kg</td> -->
+                    	<?php 
+//                     	   $oldpds = $pds;
+//                     	}?>
+                <!-- 		<td align="center">de [palier.lastMontant; block=td] &euro; à [palier.montant;strconv=no] &euro; [palier.toDelete;strconv=no]</td> -->
+<!--                 		<td><input type="text" name="newPalier[[view.contrat]]" value="" size="5" />&nbsp;kg</td> -->
+<!--                 	</tr> -->
+                	
+                	<?php
+//                     	           $entete = true;
+//                     	       }
+//                     	    ?>
+<!--                 	    <tr class="oddeven"> -->
+                	    
+                    	    <td align="left"><?php //echo $TCountry[$pays] ?></td>
+                    	    <td align="center"><?php //echo $dpt; ?></td>
+                    	    <td align="center"><?php //echo (!empty($code)) ? $code : ""; ?></td>
+<!--                     	    <td align="center">Timbre</td> -->
+                    	    <?php 
+//                     	    foreach ($prices as $pds => $prix){
+//                     	    ?>
+                    	    <!--<td align="center"><input type="text" name="newPalier[[view.contrat]]" value="<?php //echo $prix ?>" size="5" />&nbsp;€</td>-->
+                    	    <?php }?>
+                    	    <!-- 		<td align="center">de [palier.lastMontant; block=td] &euro; à [palier.montant;strconv=no] &euro; [palier.toDelete;strconv=no]</td> -->
+<!--                     	    <td>&nbsp;</td> -->
+                	    
+<!--                 	    </tr> -->
+                	    <?php 
+//                     	   }
+//                     	}
+//                 	}
+//                 	?>
+                	
+<!--                 </table> -->
+                <?php
+//             }
+//         }
+        
+    //}
 }
+
+
+?>
+
+<!--<div class="tabsAction">-->
+<!--<input type="submit" name="save" value="Enregistrer" class="button" />-->
+<!--</div>-->
+
+<?php 
+//print $form->selectarray('transports', $TTransport, '', 1);
+?>
